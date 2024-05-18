a1a8ed240ad5ffdbb786fe154bb1be664deeda59
Anwarul07 <mdanwarul064@gmail.com>
2024-05-18 23:49:36 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(" Random Password ")
+console.log(" Random Password ");
+const pass = document.getElementById("pass");
+const lenght = 12;
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@$&";
+const allchar = upper + lower + number + symbol;
+
+function createpassword() {
+    let password = "";
+    password += upper[Math.floor(Math.random() * upper.length)];
+    password += lower[Math.floor(Math.random() * lower.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+ 
+}
+
